c90870e441e99d5d5727291304d792cfea7a0730
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-16 11:29:34 +0300

    Text outlining & unoutlining.

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -24,6 +24,19 @@
                                   range:self.body.selectedRange];
 }
 
+- (IBAction)outlineBodySelection {
+    
+    [self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3,
+                                           NSStrokeColorAttributeName : [UIColor blackColor]}
+                                   range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection {
+    
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
