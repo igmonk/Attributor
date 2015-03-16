c0c8d10991ffdea846bf471983b59c5e9bbb2a58
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-16 16:53:27 +0300

    Navigation controller.

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Igor on 3/16/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
