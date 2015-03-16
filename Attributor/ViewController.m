a91920d590d44916a467fa41528d71bdc67ca679
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-16 12:50:24 +0300

    Notification centre is used to set preferred fonts.

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -30,6 +30,35 @@
                                   forState:UIControlStateNormal];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+
+    [super viewWillAppear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void) preferredFontsChanged:(NSNotification *)notification {
+
+    [self usePreferredFonts];
+}
+
+- (void) usePreferredFonts {
+    
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+}
+
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
     
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName
